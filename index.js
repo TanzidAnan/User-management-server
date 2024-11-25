ee871889bb9e8cc574dd5cca5e77b5190b1b2958
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-25 15:26:28 +0600

    data

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app =express();
 const port =process.env.PORT || 5000;
 
+const users =[
+    {id:1,name:'Tanzid',Address:'Dhaka',email:'info1@gmail.com'},
+    {id:2,name:'Marzan',Address:'Dhaka',email:'info2@gmail.com'},
+    {id:3,name:'Anan',Address:'Dhaka',email:'info3@gmail.com'},
+    {id:4,name:'Tanzid anan',Address:'Dhaka',email:'info4@gmail.com'},
+]
+
 app.get('/' ,(req,res) =>{
     res.send('user management port running')
 })
+app.get('/users',(req,res) =>{
+    res.send(users)
+})
+
 
 app.listen(port,() =>{
     console.log(`surver is running Port ${port}`)
